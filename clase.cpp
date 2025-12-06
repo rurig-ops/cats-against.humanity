125cccb2312ce63f6fbc415ae11b76d981f9f723
rurig <rurig@users.noreply.github.com>
2025-12-06 17:34:45 +0200

    reparatii1

--- clase.cpp
+++ clase.cpp
@@ -1,9 +1,10 @@
 #include "clase.h"
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
-//                                    :3
+// :3 Cat
 
 Cat::Cat(string n, int e, int c, int h, int l)
     : name(n), evilness(e), cuteness(c), hunger(h), loyalty(l) {}
@@ -70,9 +71,16 @@ void Cat::decreaseEvilness(int amount) {
     if (evilness < 0) evilness = 0;
 }
 
-//                                             :3
+void Cat::pamper(int amount) {
+    cuteness += amount;
+    if (cuteness > 100) cuteness = 100;
+    cout << name << " went to the spa! Cuteness: " << cuteness << endl;
+}
 
-Humanity::Humanity(int start, int maxS) : suspicion(start), maxSuspicion(maxS) {}
+// :3 Humanity
+
+Humanity::Humanity(int start, int maxS)
+    : suspicion(start), maxSuspicion(maxS) {}
 
 void Humanity::increaseSuspicion(int val) {
     suspicion += val;
@@ -88,20 +96,20 @@ void Humanity::decreaseSuspicion(int val) {
     cout << "Suspicion lowered: " << suspicion << "/" << maxSuspicion << endl;
 }
 
-bool Humanity::isGameOver() const { return suspicion >= maxSuspicion; }
+bool Humanity::isGameOver() const {
+    return suspicion >= maxSuspicion;
+}
 
 ostream& operator<<(ostream& os, const Humanity& h) {
     os << "Human Suspicion: " << h.suspicion << "/" << h.maxSuspicion;
     return os;
 }
 
-//                                    :3
+// :3 Mission
 
-Mission::Mission(const string& n, int diff, int money, int chaos, int hunger,
-                 int minE, int minL, int minC, MissionType t)
-    : name(n), difficulty(diff), rewardMoney(money), rewardChaos(chaos),
-      hungerCost(hunger), minEvilness(minE), minLoyalty(minL),
-      minCuteness(minC), type(t) {}
+Mission::Mission(const string& n, int diff, int money, int chaos, int hunger, int minE, int minL, int minC, MissionType t)
+    : name(n), difficulty(diff), rewardMoney(money), rewardChaos(chaos), hungerCost(hunger),
+      minEvilness(minE), minLoyalty(minL), minCuteness(minC), type(t) {}
 
 bool Mission::attempt(const Cat& c) const {
     if (type == EVIL)
@@ -111,15 +119,14 @@ bool Mission::attempt(const Cat& c) const {
 }
 
 ostream& operator<<(ostream& os, const Mission& m) {
-    os << "Mission: " << m.name
-       << " | Type: " << (m.type == Mission::EVIL ? "EVIL" : "PR")
+    os << "Mission: " << m.name << " | Type: " << (m.type == Mission::EVIL ? "EVIL" : "PR")
        << " | Difficulty: " << m.difficulty
        << " | Reward: " << m.rewardMoney
        << " | Chaos: " << m.rewardChaos;
     return os;
 }
 
-//                                              :3
+// :3 CatOverlord
 
 CatOverlord::CatOverlord(int startMoney, int startChaos, int startAP)
     : money(startMoney), chaosPoints(startChaos), actionPoints(startAP)
@@ -129,28 +136,41 @@ CatOverlord::CatOverlord(int startMoney, int startChaos, int startAP)
     actions.push_back(make_unique<RecruitCatsAction>(this));
 }
 
-void CatOverlord::addCat(const Cat& c) { cats.push_back(c); }
+void CatOverlord::addCat(const Cat& c) {
+    cats.push_back(c);
+}
 
 void CatOverlord::feedCat(int i, int cant) {
-    if (actionPoints <= 0) throw NotEnoughAPException(1, actionPoints);
-    if (i < 0 || i >= (int)cats.size()) throw InvalidCatIndexException(i);
-    if (cant > money) throw NotEnoughMoneyException(cant, money);
+    if (actionPoints <= 0)
+        throw NotEnoughAPException(1, actionPoints);
+    if (i < 0 || i >= (int)cats.size())
+        throw InvalidCatIndexException(i);
+    if (cant > money)
+        throw NotEnoughMoneyException(cant, money);
+
     cats[i].feed(cant);
     money -= cant;
     actionPoints--;
 }
 
 void CatOverlord::encourageCat(int i, int cant) {
-    if (actionPoints <= 0) throw NotEnoughAPException(1, actionPoints);
-    if (i < 0 || i >= (int)cats.size()) throw InvalidCatIndexException(i);
+    if (actionPoints <= 0)
+        throw NotEnoughAPException(1, actionPoints);
+    if (i < 0 || i >= (int)cats.size())
+        throw InvalidCatIndexException(i);
+
     cats[i].rewardLoyalty(cant);
     actionPoints--;
 }
 
 void CatOverlord::trainCatEvil(int i, int cant) {
-    if (actionPoints <= 0) throw NotEnoughAPException(1, actionPoints);
-    if (i < 0 || i >= (int)cats.size()) throw InvalidCatIndexException(i);
-    if (cant > money) throw NotEnoughMoneyException(cant, money);
+    if (actionPoints <= 0)
+        throw NotEnoughAPException(1, actionPoints);
+    if (i < 0 || i >= (int)cats.size())
+        throw InvalidCatIndexException(i);
+    if (cant > money)
+        throw NotEnoughMoneyException(cant, money);
+
     cats[i].trainEvil(cant);
     money -= cant;
     actionPoints--;
@@ -159,55 +179,182 @@ void CatOverlord::trainCatEvil(int i, int cant) {
 // interactive methods
 void CatOverlord::feedCatInteractive() {
     int index, cant;
-    cout << "Which cat to feed? "; cin >> index;
-    cout << "How much? "; cin >> cant;
+    cout << "Which cat to feed? ";
+    cin >> index;
+    cout << "How much? ";
+    cin >> cant;
     feedCat(index, cant);
 }
 
 void CatOverlord::encourageCatInteractive() {
     int index, cant;
-    cout << "Which cat to encourage? "; cin >> index;
-    cout << "How much? "; cin >> cant;
+    cout << "Which cat to encourage? ";
+    cin >> index;
+    cout << "How much? ";
+    cin >> cant;
     encourageCat(index, cant);
 }
 
 void CatOverlord::trainCatEvilInteractive() {
     int index, cant;
-    cout << "Which cat to train evilly? "; cin >> index;
-    cout << "How much? "; cin >> cant;
+    cout << "Which cat to train evilly? ";
+    cin >> index;
+    cout << "How much? ";
+    cin >> cant;
     trainCatEvil(index, cant);
 }
 
-void CatOverlord::sendOnMissionInteractive(Humanity& h) {
-    int index;
-    cout << "Which cat for mission? "; cin >> index;
-    if (index < 0 || index >= (int)cats.size()) throw InvalidCatIndexException(index);
-    cout << "Mission attempted..." << endl;
-    // simplificat
-    h.increaseSuspicion(10);
-    cats[index].increaseHunger(10);
+// :3 CatOverlord::sendOnMissionInteractive
+void CatOverlord::sendOnMissionInteractive(Humanity& humans) {
+    if (actionPoints <= 0)
+        throw NotEnoughAPException(1, actionPoints);
+
+    if (cats.empty()) {
+        cout << "No cats available for missions!" << endl;
+        return;
+    }
+
+    // afișăm pisicile
+    cout << "Select a cat for the mission:" << endl;
+    for (size_t i = 0; i < cats.size(); ++i)
+        cout << i << ": " << cats[i] << endl;
+
+    int catIndex;
+    cin >> catIndex;
+    if (catIndex < 0 || catIndex >= (int)cats.size())
+        throw InvalidCatIndexException(catIndex);
+
+    Cat& c = cats[catIndex];
+
+    // definim misiunile PR
+    struct PRMission {
+        string name;
+        int requiredCuteness;
+        int suspicionDecrease;
+        int failSuspicionIncrease;
+        int rewardMoney;
+        string successMsg;
+        string failMsg;
+    };
+    vector<PRMission> prMissions = {
+        {"Make a brainrot TikTok", 15, 25, 22, 30,
+         "Success! The kids loved this one. It's becoming the new Italian brainrot",
+         "Phew that video sucked. Never attempt editing again"},
+        {"Make AI Facebook post", 0, 35, 44, 50,
+         "Success! The post reached old people's feeds",
+         "Even the grandparents figured out it's AI and they're suing you now"}
+    };
+
+    // definim misiunile EVIL
+    struct EvilMission {
+        string name;
+        int requiredEvilness;
+        int chaosIncrease;
+        int failSuspicionIncrease;
+        string successMsg;
+        string failMsg;
+    };
+    vector<EvilMission> evilMissions = {
+        {"Steal everyone's left sock", 0, 25, 33,
+         "The sock collection is growing",
+         "You have disappointed the sock lovers in the cat society"},
+        {"Steal the laser from Magurele", 80, 50, 67,
+         "The cats will enjoy this laser",
+         "Cojocariu will remember this"}
+    };
+
+    cout << "Choose mission type:\n1. PR Mission\n2. Evil Mission\n";
+    int typeChoice;
+    cin >> typeChoice;
+
+    actionPoints-=2;
+    c.increaseHunger(10);
+
+    if (typeChoice == 1) {
+        cout << "Select PR mission:\n";
+        for (size_t i = 0; i < prMissions.size(); ++i)
+            cout << i << ". " << prMissions[i].name << endl;
+        int mIndex;
+        cin >> mIndex;
+        if (mIndex < 0 || mIndex >= (int)prMissions.size()) {
+            cout << "Invalid mission index" << endl;
+            actionPoints++; // restituim AP
+            return;
+        }
+        PRMission& m = prMissions[mIndex];
+        if (c.getCuteness() >= m.requiredCuteness) {
+            humans.decreaseSuspicion(m.suspicionDecrease);
+            money += m.rewardMoney;       // primim bani
+            cout << m.successMsg << " You earned $" << m.rewardMoney << "!" << endl;
+        } else {
+            humans.increaseSuspicion(m.failSuspicionIncrease);
+            cout << m.failMsg << endl;
+        }
+    } else if (typeChoice == 2) {
+        cout << "Select Evil mission:\n";
+        for (size_t i = 0; i < evilMissions.size(); ++i)
+            cout << i << ". " << evilMissions[i].name << endl;
+        int mIndex;
+        cin >> mIndex;
+        if (mIndex < 0 || mIndex >= (int)evilMissions.size()) {
+            cout << "Invalid mission index" << endl;
+            actionPoints++; // restituim AP
+            return;
+        }
+        EvilMission& m = evilMissions[mIndex];
+        if (c.getEvilness() >= m.requiredEvilness) {
+            chaosPoints += m.chaosIncrease;
+            cout << m.successMsg << endl;
+        } else {
+            humans.increaseSuspicion(m.failSuspicionIncrease);
+            cout << m.failMsg << endl;
+        }
+    } else {
+        cout << "Invalid mission type" << endl;
+        actionPoints++; // dacă alegerea invalidă, restituim AP
+    }
 }
 
+void CatOverlord::sendCatToSpaInteractive() {
+    int index, amount;
+    cout << "Which cat to send to the spa? ";
+    cin >> index;
+    cout << "How much pampering? ";
+    cin >> amount;
+
+    if (actionPoints <= 0)
+        throw NotEnoughAPException(1, actionPoints);
+    if (index < 0 || index >= (int)cats.size())
+        throw InvalidCatIndexException(index);
+
+    cats[index].pamper(amount);
+    actionPoints--;
+}
+
+
 void CatOverlord::performAction(int catIndex, int actionIndex, Humanity& h) {
-    if (catIndex < 0 || catIndex >= (int)cats.size()) throw InvalidCatIndexException(catIndex);
+    if (catIndex < 0 || catIndex >= (int)cats.size())
+        throw InvalidCatIndexException(catIndex);
     if (actionIndex < 0 || actionIndex >= (int)actions.size()) {
         cout << "Invalid action index\n";
         return;
     }
 
-    cout << "Cat " << cats[catIndex].getName() << " is performing action: "
-         << actions[actionIndex]->name() << "\n";
+    cout << "Cat " << cats[catIndex].getName()
+         << " is performing action: " << actions[actionIndex]->name() << "\n";
+
     actions[actionIndex]->perform(cats[catIndex], h);
 }
 
-
 void CatOverlord::nextDay() {
     actionPoints = 3;
-    for (auto &c : cats) c.increaseHunger(5);
+    for (auto &c : cats)
+        c.increaseHunger(5);
 }
 
 void CatOverlord::printStatus() const {
-    cout << "Money: " << money << " | Chaos: " << chaosPoints
+    cout << "Money: " << money
+         << " | Chaos: " << chaosPoints
          << " | AP: " << actionPoints << endl;
 }
 
@@ -216,14 +363,14 @@ void CatOverlord::printCats() const {
         cout << i << ": " << cats[i] << endl;
 }
 
-// utility to sort cats by evilness
 void CatOverlord::sortCatsByEvilness() {
-    sort(cats.begin(), cats.end(), [](const Cat &a, const Cat &b){
+    sort(cats.begin(), cats.end(), [](const Cat &a, const Cat &b) {
         return a.getEvilness() > b.getEvilness();
     });
 }
 
-// ---------------- StealFoodAction ----------------
+// :3 StealFoodAction
+
 StealFoodAction::StealFoodAction() = default;
 
 void StealFoodAction::execute(Cat& c, Humanity& h) {
@@ -233,12 +380,10 @@ void StealFoodAction::execute(Cat& c, Humanity& h) {
 }
 
 string StealFoodAction::name() const { return "Steal Food"; }
+unique_ptr<CatAction> StealFoodAction::clone() const { return make_unique<StealFoodAction>(*this); }
 
-unique_ptr<CatAction> StealFoodAction::clone() const {
-    return make_unique<StealFoodAction>(*this);
-}
+// :3 SpreadChaosAction
 
-// ---------------- SpreadChaosAction ----------------
 SpreadChaosAction::SpreadChaosAction() = default;
 
 void SpreadChaosAction::execute(Cat& c, Humanity& h) {
@@ -248,12 +393,10 @@ void SpreadChaosAction::execute(Cat& c, Humanity& h) {
 }
 
 string SpreadChaosAction::name() const { return "Spread Chaos"; }
+unique_ptr<CatAction> SpreadChaosAction::clone() const { return make_unique<SpreadChaosAction>(*this); }
 
-unique_ptr<CatAction> SpreadChaosAction::clone() const {
-    return make_unique<SpreadChaosAction>(*this);
-}
+// :3 RecruitCatsAction
 
-// ---------------- RecruitCatsAction ----------------
 RecruitCatsAction::RecruitCatsAction(CatOverlord* o) : overlord(o) {}
 
 void RecruitCatsAction::execute(Cat& c, Humanity& h) {
@@ -266,8 +409,4 @@ void RecruitCatsAction::execute(Cat& c, Humanity& h) {
 }
 
 string RecruitCatsAction::name() const { return "Recruit Cats"; }
-
-unique_ptr<CatAction> RecruitCatsAction::clone() const {
-    return make_unique<RecruitCatsAction>(*this);
-}
-
+unique_ptr<CatAction> RecruitCatsAction::clone() const { return make_unique<RecruitCatsAction>(*this); }
--- clase.h
+++ clase.h
@@ -1,14 +1,13 @@
 #ifndef CLASE_H
 #define CLASE_H
-
 #include <iostream>
 #include <vector>
 #include <string>
 #include <memory>
 #include <exception>
-using namespace std;
+#include <algorithm>
 
-//                                        :3
+using namespace std;
 
 class Cat {
 private:
@@ -17,7 +16,6 @@ private:
     int cuteness;
     int hunger;
     int loyalty;
-
 public:
     Cat(string n, int e, int c, int h, int l);
     explicit Cat(string n);
@@ -25,44 +23,34 @@ public:
     ~Cat();
     Cat& operator=(const Cat& other);
     friend ostream& operator<<(ostream& os, const Cat& c);
-
     [[nodiscard]] const string& getName() const;
     [[nodiscard]] int getEvilness() const;
     [[nodiscard]] int getCuteness() const;
     [[nodiscard]] int getHunger() const;
     [[nodiscard]] int getLoyalty() const;
-
     void feed(int cant);
     void trainEvil(int cant);
     void rewardLoyalty(int cant);
     void increaseHunger(int cant);
     void decreaseEvilness(int amount);
+    void pamper(int amount);
 };
-
-//                                           :3
 
 class Humanity {
 private:
     int suspicion;
     int maxSuspicion;
-
 public:
     explicit Humanity(int start = 0, int maxS = 100);
-
     void increaseSuspicion(int val);
     void decreaseSuspicion(int val);
-
     [[nodiscard]] bool isGameOver() const;
-
     friend ostream& operator<<(ostream& os, const Humanity& h);
 };
-
-//                                         :3
 
 class Mission {
 public:
     enum MissionType { EVIL, PR };
-
 private:
     string name;
     int difficulty;
@@ -73,7 +61,6 @@ private:
     int minLoyalty;
     int minCuteness;
     MissionType type;
-
 public:
     Mission(const string& n, int diff, int money, int chaos, int hunger, int minE, int minL, int minC, MissionType t);
     [[nodiscard]] const string& getName() const { return name; }
@@ -82,80 +69,55 @@ public:
     [[nodiscard]] int getHungerCost() const { return hungerCost; }
     [[nodiscard]] int getDifficulty() const { return difficulty; }
     [[nodiscard]] MissionType getType() const { return type; }
-
     [[nodiscard]] bool attempt(const Cat& c) const;
-
     friend ostream& operator<<(ostream& os, const Mission& m);
 };
 
-//                                          :3
-
+class CatAction;
 class CatOverlord {
 private:
     vector<Cat> cats;
     int money;
     int chaosPoints;
     int actionPoints;
-
-    vector<unique_ptr<class CatAction>> actions; // vector de actiuni va fi initializat in cpp
-
+    vector<unique_ptr<CatAction>> actions;
 public:
     explicit CatOverlord(int startMoney = 50, int startChaos = 10, int startAP = 6);
-
     friend ostream& operator<<(ostream& os, const CatOverlord& o);
-
     void addCat(const Cat& c);
     void feedCat(int index, int cant);
     void encourageCat(int index, int cant);
     void nextDay();
     bool sendOnMission(int index, const Mission& m, Humanity& humans);
     void trainCatEvil(int index, int cant);
-
     void printStatus() const;
     void printCats() const;
-
     void feedCatInteractive();
     void encourageCatInteractive();
     void trainCatEvilInteractive();
     void sendOnMissionInteractive(Humanity& humans);
     void calmCatInteractive();
-
     void sortCatsByEvilness();
     bool checkEvilnessGameOver();
-
     vector<Cat>& getCats() { return cats; }
-
     void performAction(int catIndex, int actionIndex, Humanity& humans);
     size_t getNumActions() { return actions.size(); }
     vector<unique_ptr<CatAction>>& getActions() { return actions; }
+    void sendCatToSpaInteractive();
 };
-
-//                                         :3
 
 class CatAction {
 public:
     virtual ~CatAction() = default;
-
-    // funcție virtuală pură, implementată de fiecare acțiune concretă
     virtual void execute(Cat& c, Humanity& h) = 0;
-
-    // returnează numele acțiunii
     virtual string name() const = 0;
-
-    // clone pentru a permite copierea în vectorul de unique_ptr
     virtual unique_ptr<CatAction> clone() const = 0;
-
-    // interfață non-virtuală care poate fi folosită de main
-    void perform(Cat& c, Humanity& h) {
-        cout << c.getName() << " is performing action: " << name() << endl;
-        execute(c, h);
-    }
+    void perform(Cat& c, Humanity& h) { cout << c.getName() << " is performing action: " << name() << endl; execute(c, h); }
 };
 
 class StealFoodAction : public CatAction {
 public:
-    StealFoodAction();  // doar declarație
-
+    StealFoodAction();
     void execute(Cat& c, Humanity& h) override;
     string name() const override;
     unique_ptr<CatAction> clone() const override;
@@ -164,7 +126,6 @@ public:
 class SpreadChaosAction : public CatAction {
 public:
     SpreadChaosAction();
-
     void execute(Cat& c, Humanity& h) override;
     string name() const override;
     unique_ptr<CatAction> clone() const override;
@@ -175,13 +136,11 @@ private:
     CatOverlord* overlord;
 public:
     explicit RecruitCatsAction(CatOverlord* o);
-
     void execute(Cat& c, Humanity& h) override;
     string name() const override;
     unique_ptr<CatAction> clone() const override;
 };
 
-//                                                                                :3
 class GameException : public exception {
 protected:
     string message;
@@ -192,22 +151,17 @@ public:
 
 class InvalidCatIndexException : public GameException {
 public:
-    explicit InvalidCatIndexException(int index)
-        : GameException("Invalid cat index: " + to_string(index)) {}
+    explicit InvalidCatIndexException(int index) : GameException("Invalid cat index: " + to_string(index)) {}
 };
 
 class NotEnoughMoneyException : public GameException {
 public:
-    explicit NotEnoughMoneyException(int cost, int money)
-        : GameException("Not enough money! Needed: " + to_string(cost) +
-                        ", Available: " + to_string(money)) {}
+    explicit NotEnoughMoneyException(int cost, int money) : GameException("Not enough money! Needed: " + to_string(cost) + ", Available: " + to_string(money)) {}
 };
 
 class NotEnoughAPException : public GameException {
 public:
-    explicit NotEnoughAPException(int needed, int available)
-        : GameException("Not enough action points! Needed: " + to_string(needed) +
-                        ", Available: " + to_string(available)) {}
+    explicit NotEnoughAPException(int needed, int available) : GameException("Not enough action points! Needed: " + to_string(needed) + ", Available: " + to_string(available)) {}
 };
 
 #endif
